47c8141305ed08c96d2728e1d879c5db376d249c
Fluorurine <votruong830430073@gmail.com>
2023-03-27 12:53:31 +0700

    Hoan thanh form dang nhap va dang ky

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,9 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
+  <h1 class="text-red">Hello mn nhá</h1>
+  <RouterLink to="login">Đăng nhập</RouterLink>
+  <RouterLink to="register">Đăng ký</RouterLink>
   <RouterView />
 </template>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import TestComp from '@/components/TestComp.vue'
+import FormDangNhap from '@/views/FormDangNhap.vue'
+import FormDangNhap from '@/views/FormDangKy.vue'
+import TestComp from '@/views/TestComp.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: '/login',
+      name: 'login',
+      component: FormDangNhap
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: FormDangKy
+    },
     {
       path: '/',
       name: 'home',
